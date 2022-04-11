3ba3e19737644965d4b436873b05dcc7eb6c4b8f
kevin-haas <kevinhaas46@gmail.com>
2022-04-11 15:54:23 -0400

    added an operate function that takes two values and an operator

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(method, a, b) {
+    switch(method) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+    }
+}
+
 console.log(add(2, 2));
 console.log(subtract(2, 2));
 console.log(multiply(2, 2));
 console.log(divide(2, 2));
+console.log(operate('/', 2, 2));
